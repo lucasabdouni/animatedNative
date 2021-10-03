fb7e4b5bdf766648184cfbb53e69b8e7b13e1da5
Lucas <lucasabdouni@hotmail.com>
2021-10-03 12:54:28 -0300

    frist commit

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
+
+const WIDTH = Dimensions.get('window').width;
 
 export default function App() {
+  // utilizado para alterar o valor das animações de forma performatica
+  let animation = useSharedValue(0);
+
+  // Stylizar animações
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          // controla o movimento da animação no eixo x
+          translateX: withTiming(animation.value, {
+            // controla a duração
+            duration: 500,
+            // gera efeitos, pode utilizar os da biblioca ou criar no cubic-bezier.com
+            easing: Easing.bezier(0.12, 2.02, 0, 2.02),
+          }),
+        },
+      ],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (WIDTH - 200);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Passar o animatedStyles junto a stylização do componente */}
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <TouchableOpacity style={styles.button} onPress={handleAnimationPosition}>
+        <Text>Pressione</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +55,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1a1a1a',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+
+  button: {
+    marginTop: 10,
+    padding: 5,
+
+    alignItems: 'center',
+
+    backgroundColor: '#F0FFFF',
+
+    borderRadius: 5,
   },
 });
